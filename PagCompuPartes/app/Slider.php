ef485fea1edfad01e10c8f22a5a3cbe251b18a9d
jazmin valencia <jazminvalencia@MacBook-Air-de-jazmin.local>
2020-03-17 16:03:24 -0700

    web y Slider.php y producto-php

--- PagCompuPartes/app/Slider.php
+++ PagCompuPartes/app/Slider.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Slider extends Model
 {
-    //
+   protected $primaryKey = 'idSlider';
+   protected $fillable = 
+   [
+    'nombre', 'titulo','texto','img'
+   ];
 }
