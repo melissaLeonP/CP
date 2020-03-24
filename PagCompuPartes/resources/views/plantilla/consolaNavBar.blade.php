0d5ef3aeb9818db367dd0311f6e8fe53c459e152
jazminValencia <jvalencia@powersoftec.com>
2020-03-24 11:53:46 -0700

    nav bien

--- PagCompuPartes/resources/views/plantilla/consolaNavBar.blade.php
+++ PagCompuPartes/resources/views/plantilla/consolaNavBar.blade.php
@@ -1,9 +1,10 @@
 <nav  class="responsive">
   <div class="fixed finish nav-wrapper">
   <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-    <ul class="right">
+  <a href="#" class="hide-on-small-only"><img id="navImagen" class="" src="img/logoBlanco.png" style="" alt=""></a>
+    <ul class="right hide-on-small-only">
       <li @click="menu=0"><a>Sliders</a></li>
-      <li @click="menu=1"><a>Productos</a></li>
+      <li @click="menu=1"><a >Productos</a></li>
       <li @click="menu=2"><a>Categorias</a></li>
       <li @click="menu=3"><a>Cambiar contraseña </a></li>
       <li>
@@ -17,8 +18,8 @@
   </nav>
 
 
-<ul class="sidenav left" id="mobile-demo">
-    <li><img class="logoNav centro" src="img/logo-250x50.png" alt=""></li>
+<ul class="sidenav left " id="mobile-demo">
+    <li><img class="center" src="img/logo-250x50.png" alt=""></li>
     <li @click="menu=0"><a class="black-text" href="#">Sliders</a></li>
     <li @click="menu=1"><a class="black-text" href="#">Productos</a></li>
     <li @click="menu=2"><a class="black-text" href="#">Categorias</a></li>
@@ -26,8 +27,8 @@
     <!-- <li><a href="collapsible.html">cerrar</a></li> -->
     <li>
       <a onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Cerrar Sesión</a>
-        </li>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" >
-        {{ csrf_field() }}
-        </form>
+    </li>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" >
+    {{ csrf_field() }}
+    </form>
 </ul>
